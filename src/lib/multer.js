26243472c02608ecd179f3505bb9a7a8613b91c3
brunozapico <brunoezapico@gmail.com>
2020-08-10 10:28:22 -0300

    user/contact multer

--- src/lib/multer.js
+++ src/lib/multer.js
@@ -1,11 +1,21 @@
 const path = require('path');
 const multer = require('multer');
 
-module.exports = multer.diskStorage({
+module.exports = {
+  user_multer: multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, path.resolve(__dirname, '..', 'public', 'img', 'user', 'avatar'));
     },
     filename: function (req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
     },
-});
+  }),
+  contacts_multer: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '..', 'public', 'img', 'contacts', 'avatar'));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    },
+  }),
+};
